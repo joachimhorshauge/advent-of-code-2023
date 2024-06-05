79ed8f14f2541f776dd9e49830672c40458b5d7f
Joachim Horshauge <jh.hors@gmail.com>
2024-06-05 03:26:17 +0200

    [Added] Solution for day 04 part 2

--- cmd/day04/main.go
+++ cmd/day04/main.go
@@ -7,8 +7,16 @@ import (
 	"math"
 	"os"
 	"regexp"
+	"strconv"
 	"strings"
 )
+
+type Scratchcard struct {
+	CardNumber int
+	A          []string
+	B          []string
+	amount     int
+}
 
 func main() {
 	// Read input file
@@ -23,11 +31,38 @@ func main() {
 	fmt.Println("Part 2:", part2(input))
 }
 
-func evalScratchcard(scratchcard []string) int {
+func evalScratchcard(card Scratchcard) int {
+	return len(intersect.Hash(card.A, card.B))
+}
+
+func parseScratchcard(line string) Scratchcard {
 	re := regexp.MustCompile(`\d{1,2}`)
+	parts := strings.Split(line, ":")
+	cardId := parts[0]
+	cardNumber, _ := strconv.Atoi(strings.TrimSpace(cardId[5:])) // Remove "Card " prefix
+
+	scratchcard := strings.Split(parts[1], "|")
 	a := re.FindAllString(scratchcard[0], -1)
 	b := re.FindAllString(scratchcard[1], -1)
-	return int(math.Pow(2, float64(len(intersect.Hash(a, b)))-1))
+
+	return Scratchcard{
+		CardNumber: cardNumber,
+		A:          a,
+		B:          b,
+		amount:     1,
+	}
+}
+
+func parseAllScratchcards(input string) map[int]Scratchcard {
+	lines := strings.Split(input, "\n")
+	scratchcards := make(map[int]Scratchcard)
+
+	for _, line := range lines {
+		card := parseScratchcard(line)
+		scratchcards[card.CardNumber] = card
+	}
+
+	return scratchcards
 }
 
 func part1(input string) string {
@@ -35,15 +70,26 @@ func part1(input string) string {
 
 	result := 0
 	for _, line := range lines {
-		line = strings.Split(line, ":")[1]      // Remove prefix
-		scratchcard := strings.Split(line, "|") // Remove prefix
-		result += evalScratchcard(scratchcard)
+		card := parseScratchcard(line)
+		result += int(math.Pow(2, float64(evalScratchcard(card))-1))
 	}
 
 	return fmt.Sprintf("%d", result)
 }
 
 func part2(input string) string {
-	// Your Part 2 code here
-	return "Result of Part 2"
+	scratchcards := parseAllScratchcards(input)
+	result := 0
+	for i := 1; i <= len(scratchcards); i++ {
+		result += scratchcards[i].amount
+		n := evalScratchcard(scratchcards[i])
+		for j := 1; j <= n; j++ {
+			if i+j <= len(scratchcards) {
+				tempCard := scratchcards[i+j]
+				tempCard.amount += scratchcards[i].amount
+				scratchcards[i+j] = tempCard
+			}
+		}
+	}
+	return fmt.Sprintf("%d", result)
 }
